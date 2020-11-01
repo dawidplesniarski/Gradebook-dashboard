4486138b815e3a3220d26d9a03d777793921ebc9
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-01 14:09:35 +0100

    AllUsersTable component added

--- src/pages/admin/AdminStudentsPage.js
+++ src/pages/admin/AdminStudentsPage.js
@@ -5,6 +5,10 @@ import {withRouter} from "react-router";
 import Burger from "../../components/Molecules/Hamburger/Burger";
 import BackButton from "../../components/Atoms/BackButton/BackButton";
 import AddStudentForm from "../../components/Forms/AddStudentForm/AddStudentForm";
+import axios from 'axios';
+import {API_URL} from "../../utils/helpers";
+import AddButton from "../../components/Atoms/AddButton/AddButton";
+import AllUsersTable from "../../components/Tables/AllUsersTable";
 
 const StyledMainPageContainer = styled.div`
   display: flex;
@@ -17,17 +21,29 @@ const StyledMainPageContainer = styled.div`
 
 const AdminStudentsPage = ({loginReducer, history}) => {
     const [studentsData, setStudentsData] = useState([]);
+    const [formVisible, toggleForm] = useState(false);
 
     const fetchAllStudents = () => {
-
+        try {
+            axios.get(`${API_URL}/users/findAll`).then(res => {
+               setStudentsData(res.data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
+
+    useEffect(() => {
+        fetchAllStudents();
+    },[]);
 
     return(
         <>
             <Burger isAdminOpened={true}/>
             <BackButton onClick={() => history.push('/adminMainPage')}/>
+            <AddButton onClick={() => toggleForm(!formVisible)} open={formVisible}/>
             <StyledMainPageContainer>
-                <AddStudentForm/>
+                {!formVisible ? <AllUsersTable data={studentsData}/> : <AddStudentForm/>}
             </StyledMainPageContainer>
         </>
     );
