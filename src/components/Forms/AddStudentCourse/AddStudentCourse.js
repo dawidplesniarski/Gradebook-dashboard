ab7d02e1ea30c3631c0643205aaee577c1b5c7d6
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-05 19:37:56 +0100

    AddNewCourse styles extracted

--- src/components/Forms/AddStudentCourse/AddStudentCourse.js
+++ src/components/Forms/AddStudentCourse/AddStudentCourse.js
@@ -1,51 +1,12 @@
 import React, {useState, useEffect} from "react";
-import styled from 'styled-components';
 import axios from 'axios';
 import {API_URL, TOKEN} from "../../../utils/helpers";
 import SelectCourseMenu from "../../Atoms/SelectCourseMenu/SelectCourseMenu";
 import Button from "../../Atoms/Button/Button";
 import {connect} from 'react-redux';
-import {studentReducer} from "../../../reducers/studentReducer";
 import AlertComponent from "../../Atoms/Alert/Alert";
+import {StyledFormTitle, StyledContentWrapper, AddStudentCourseWrapper} from './AddStudentCourse.styles';
 
-const AddStudentCourseWrapper = styled.div`
-  margin-top: 30px;
-  margin-bottom: 30px;
-  background-color: #fafafa;
-  border-radius: 15px;
-  width: 700px;
-  display: flex;
-  flex-direction: column;
-  padding: 0.25rem;
-  align-items: center;
-  @media (max-width: 1000px) {
-    width: 500px;
-  }
-  @media (max-width: 800px) {
-    width: 400px;
-  }
-  @media (max-width: 700px) {
-    width: 350px;
-  }
-`;
-
-const StyledContentWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 200px;
-  width: 80%;
-`;
-
-const StyledFormTitle = styled.p`
-  font-family: Montserrat,serif;
-  font-weight: 500;
-  font-size: 25px;
-  @media(max-width: 768px) {
-    font-size: 20px;
-  }
-`;
 
 const AddStudentCourse = ({studentReducer}) => {
     const [courseId, setCourseId] = useState(null);
---
+++ src/components/Forms/AddStudentCourse/AddStudentCourse.styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const AddStudentCourseWrapper = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+  background-color: #fafafa;
+  border-radius: 15px;
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  padding: 0.25rem;
+  align-items: center;
+  @media (max-width: 1000px) {
+    width: 500px;
+  }
+  @media (max-width: 800px) {
+    width: 400px;
+  }
+  @media (max-width: 700px) {
+    width: 350px;
+  }
+`;
+
+const StyledContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 200px;
+  width: 80%;
+`;
+
+const StyledFormTitle = styled.p`
+  font-family: Montserrat,serif;
+  font-weight: 500;
+  font-size: 25px;
+  @media(max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+export {StyledFormTitle, StyledContentWrapper, AddStudentCourseWrapper};
