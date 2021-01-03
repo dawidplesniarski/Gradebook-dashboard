bd7034d9b1320c35877eb6da72ca0545f45244db
dawidplesniarski <dawidplesniarski@gmail.com>
2021-01-03 18:10:40 +0100

    TextInput test added

--- src/tests/TextInput.test.js
+++ src/tests/TextInput.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import {act, fireEvent, render} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import TextInput from "../components/Atoms/TextInput/TextInput";
 
@@ -8,3 +8,13 @@ test('Should render TextInput', () => {
     const {getByDisplayValue} = render(<TextInput onChange={() => 'test'} type={'text'} name={'test'} defaultValue={'Test'}/>)
     expect(getByDisplayValue('Test')).toBeInTheDocument()
 });
+
+test('Should enter text into the text input', () => {
+    let result = null;
+    const {getByDisplayValue} = render(<TextInput onChange={e => result = e.target.value} type={'text'} name={'Test'} defaultValue={'Input'}/>)
+    const input = getByDisplayValue('Input');
+    act(() => {
+        fireEvent.change(input, {target: {value: 'Success message'}})
+    })
+    expect(result).toBe('Success message');
+})
